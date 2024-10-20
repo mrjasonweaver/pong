430fbbfafe7a26ec22e2dc4eabba8f6ecc8e7e3d
Jason Weaver <indyplanets@gmail.com>
2024-10-19 21:00:42 -0500

    learning the Entity Component System

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_systems(Startup, add_people)
-        .add_systems(Update, hello_world)
+        .add_systems(Update, (hello_world, (update_people, great_people).chain()))
         .run();
 }
 
@@ -21,4 +21,19 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Elaina Proctor".to_string())));
     commands.spawn((Person, Name("Renzo Hume".to_string())));
     commands.spawn((Person, Name("Zeyna Nieves".to_string())));
+}
+
+fn great_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}", name.0);
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Elaina Proctor" {
+            name.0 = "Elain Hume".to_string();
+            break;
+        }
+    }
 }
